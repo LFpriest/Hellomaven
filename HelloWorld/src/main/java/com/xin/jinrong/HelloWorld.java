067e0f842b78073cafc0d63f8be608e9f3485900
LFpriest <1033249488@qq.com>
2019-01-18 15:42:45 +0800

    发布项目

---
+++ HelloWorld/src/main/java/com/xin/jinrong/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.xin.jinrong;
+
+public class HelloWorld {
+	
+	public String sayHello(String name){
+		return "Hello "+name ;
+	}
+	
+//	public void main(String[] args){
+//		
+//		System.out.println(sayHello("zs"));
+//	}
+
+}
---
+++ HelloWorld/src/test/java/com/xin/jinrong/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.xin.jinrong;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void testHello(){
+		HelloWorld hello = new HelloWorld();
+		String result = hello.sayHello("zs");
+		assertEquals("Hello zs",result);	//断言
+	}
+
+}
